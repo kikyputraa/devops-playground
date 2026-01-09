eda54c6dadc02dcd163c801cea8878f57804f923
Rizki Putra <rputrar@assistxenterprise.ai>
2026-01-09 14:18:28 +0700

    Adding: Kubernetes with Minikube

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.main("/")
+@app.route("/")  # Pastikan menggunakan .route BUKAN .main
 def hello():
-    return "Hello DevOps! Aplikasi Python ini berjalan di Docker."
+    return "Hello, ini aplikasi Flask yang berjalan di Kubernetes!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000) # nosec
+    app.run(host='0.0.0.0', port=5000)
