46e9e2d9128a69e132d784d2fd406bcdd4e53775
Rizki Putra <rputrar@assistxenterprise.ai>
2026-01-09 09:18:29 +0700

    Updating

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
-FROM python:3.11-slim
-
+FROM python:3.9-slim
 WORKDIR /app
-COPY app/ .
-RUN pip install flask
-
-EXPOSE 5000
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+COPY . .
 CMD ["python", "app.py"]
--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+@app.main("/")
 def hello():
-    return "Hello DevOps World 🚀"
+    return "Hello DevOps! Aplikasi Python ini berjalan di Docker."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
