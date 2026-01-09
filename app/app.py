c0f85cd0fe1daa61aaea0f73f35e6a8aa6a8dbdc
Rizki Putra <rputrar@assistxenterprise.ai>
2026-01-09 09:40:12 +0700

    Fix: Ignore Sast port scan

--- app/app.py
+++ app/app.py
@@ -6,4 +6,4 @@ def hello():
     return "Hello DevOps! Aplikasi Python ini berjalan di Docker."
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000) # nosec
