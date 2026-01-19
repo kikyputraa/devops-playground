4d6e2be5015727380b22fe9ff5ae5e4592dda4f9
Rizki Putra <rputrar@assistxenterprise.ai>
2026-01-19 09:42:24 +0700

    fix: fix app /health to pass unit test

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ def hello():
 # TAMBAHKAN INI AGAR UNIT TEST PASS
 @app.route('/health')
 def health():
-    return jsonify({"status": "healthy"}), 200
+    return jsonify({"status": "ok"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000) #nosec
