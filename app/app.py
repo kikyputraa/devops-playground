588a5076844e2597d8d83e743bffe2274f7615b9
Rizki Putra <rputrar@assistxenterprise.ai>
2026-01-19 09:38:38 +0700

    chore: Adding Canary Env

--- app/app.py
+++ app/app.py
@@ -1,15 +1,19 @@
-from flask import Flask
+import os
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Aplikasi Sederhana DevOps Playground - Status: Berhasil Terkoneksi!"
+# Mengambil info versi dari Environment Variable (Default: V1-Stable)
+APP_VERSION = os.getenv("APP_VERSION", "V1-Stable")
 
-@app.route('/health')
-def health():
-    return {"status": "ok"}
+@app.route('/')
+def hello():
+    return jsonify({
+        "status": "success",
+        "message": "DevOps Playground API",
+        "version": APP_VERSION,
+        "environment": "Production"
+    })
 
 if __name__ == '__main__':
-    # pragma: no cover
     app.run(host='0.0.0.0', port=5000)  # nosec
